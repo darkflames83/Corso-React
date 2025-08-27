57d99e44c5cd8fa6e9ab4ca3d9e7074928fedb81
Williams Bonomi <williamsbonomi@64352186-2588-4788-a3a9-2b412cc7242a.fritz.box>
2025-08-27 22:57:01 +0200

    modifica della pagina about

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,10 +1,14 @@
 import Navbar from "../Navbar";
+import { neon } from "@netlify/neon";
+const nome = "New York";
+const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
+const [post] = await sql`SELECT * FROM Citta WHERE id = ${nome}`;
 
 function About() {
   return (
     <>
       <Navbar></Navbar>
-      <h1 className="text-3xl font-bold">Pagina About</h1>
+      <h1 className="text-3xl font-bold">Pagina About!</h1>
     </>
   );
 }
