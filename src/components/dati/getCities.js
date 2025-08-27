d41fcf42c1cf1838bb3d15717baf51fa5ff9a718
Williams Bonomi <williamsbonomi@64352186-2588-4788-a3a9-2b412cc7242a.fritz.box>
2025-08-27 23:13:06 +0200

    aggiunta di tutte le città alla pagina about

--- src/components/dati/getCities.js
+++ src/components/dati/getCities.js
@@ -3,7 +3,6 @@ import { neon } from "@netlify/neon";
 const sql = neon(); // usa automaticamente la variabile d'ambiente NETLIFY_DATABASE_URL
 
 export default async function handler(req, res) {
-  const nome = "New York";
-  const rows = await sql`SELECT * FROM Citta WHERE nome = ${nome}`;
+  const rows = await sql`SELECT * FROM Citta`;
   return res.json(rows);
 }
